5ef58f4993b406cff575a12f536c323bd8746b10
maxon <m.faleev@innopolis.university>
2020-10-20 19:31:34 +0300

    +habit api

--- social_backend/api/views.py
+++ social_backend/api/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 from rest_framework.response import Response
 from rest_framework import generics
 
-from .serializers import *
+from .serializers import UserSerializer
 
+from tracker.models import Habit    
+from tracker.serializers import HabitSerializer
 
 class UsersListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class HabitListView(generics.ListAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
--- social_backend/tracker/models.py
+++ social_backend/tracker/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Habit(models.Model):
     habit_name = models.CharField(max_length=100)
     
-    users = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name = 'habits', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.habit_name
