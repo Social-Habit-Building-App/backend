68210a69e5724d10798b7e3af55607a02d0d35b1
k05m0navt <gubaydullin.dan@gmail.com>
2020-11-07 21:43:55 +0300

    jwt auth with habits

--- social_backend/tracker/models.py
+++ social_backend/tracker/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
-from django.contrib.auth.models import User
+from accounts.models import userProfile
+#from django.contrib.auth.models import User
 
 
 class Habit(models.Model):
     habit_name = models.CharField(max_length=100)
-    
-    user = models.ForeignKey(User, related_name = 'habits', on_delete=models.CASCADE)
-    
+
+    user = models.ForeignKey(
+        userProfile, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.habit_name
