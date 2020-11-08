dae56d969e01c2775b450560c841cb5b2ccea4fe
maxon <m.faleev@innopolis.university>
2020-11-08 18:46:23 +0300

    progress on habits

--- social_backend/api/urls.py
+++ social_backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from .views import UsersListView, UserDetailView, HabitListView
+from .views import UsersListView, UserDetailView, HabitListView, habitProgress
 
 
 urlpatterns = [
     path('users/all', UsersListView.as_view()),
     path('users/<int:pk>/', UserDetailView.as_view()),
+    path('users/<int:pk>/habits/<slug:habit_name>/', habitProgress),
     path('habits/all', HabitListView.as_view()),
 ]
--- social_backend/api/views.py
+++ social_backend/api/views.py
@@ -1,24 +1,42 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, HabitSerializer
 
-from tracker.models import Habit    
-from tracker.serializers import HabitSerializer
+from tracker.models import Habit, Progress  
 
 class UsersListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-
 class UserDetailView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-
 class HabitListView(generics.ListAPIView):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+
+@api_view(['GET', 'POST'])
+def habitProgress(request, pk, habit_name):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        user = User.objects.get(pk=pk)
+        habit = user.habits.get_queryset().get(habit_name=habit_name)
+        serializer = HabitSerializer(habit)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        user = User.objects.get(pk=pk)
+        habit = user.habits.get_queryset().get(habit_name=habit_name)
+        print(request.GET)
+        progress = Progress(units_value=request.GET['units_value'], habit=habit)
+        progress.save()
+        return Response(progress.pub_date, status=status.HTTP_201_CREATED)
